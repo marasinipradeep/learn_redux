dea442cf42421e9e8c634160a6e7af59a31ee2cf
marasinipradeep <marasinipradeep@gmail.com>
2021-02-18 13:01:52 +1030

    Comments component added

---
+++ Learn-Redux-Starter-Files-master/learn-redux/client/Components/Comments.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Comments(props) {
+    
+
+    return (
+        <div className="comment">
+            I'm comments
+            
+        </div>
+    )
+}
+
+export default Comments;
