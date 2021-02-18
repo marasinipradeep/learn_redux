2888badb72cae163e8ddc386044b0a50e98854cb
marasinipradeep <marasinipradeep@gmail.com>
2021-02-18 10:56:16 +1030

    Creating Router and Routind completed

--- Learn-Redux-Starter-Files-master/learn-redux/client/reduxstagram.js
+++ Learn-Redux-Starter-Files-master/learn-redux/client/reduxstagram.js
@@ -1,1 +1,24 @@
 // let's go!
+
+import React from 'react';
+import { render } from 'react-dom';
+import css from './styles/style.styl';
+
+//Import Components
+import Main from "./Components/Main";
+import Single from "./Components/Single";
+import PhotoGrid from "./Components/PhotoGrid";
+
+//import react router deps
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+const router =(
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router,document.getElementById("root"));
