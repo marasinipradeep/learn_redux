7b4ee7aaf0ed48a5974a8da7cd0c9503abb11d1e
marasinipradeep <marasinipradeep@gmail.com>
2021-02-18 11:38:04 +1030

    Redux store with actions and reducer with combined reducer created

---
+++ Learn-Redux-Starter-Files-master/learn-redux/client/reducers/comments.js
@@ -0,0 +1,8 @@
+//initially state is empty
+
+function comments(state=[],action){
+
+    return state;
+}
+
+export default comments;
---
+++ Learn-Redux-Starter-Files-master/learn-redux/client/reducers/posts.js
@@ -0,0 +1,12 @@
+// a reducer takes in two things:
+
+//1. the action (info about what happened)
+//2. copy of current state
+
+
+//initially state is empty
+function posts(state=[],action){
+
+}
+
+export default posts;
